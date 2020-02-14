e6d982e2ebd5735dee411ab19794c7d4ad3cb5cf
Kim Paulissen <kim.paulissen@kuleuven.be>
2020-02-14 13:29:53 +0100

    add interfaces

--- src/starzel/votable_behavior/interfaces.py
+++ src/starzel/votable_behavior/interfaces.py
@@ -1,8 +1,66 @@
-# -*- coding: utf-8 -*-
-"""Module where all interfaces, events and exceptions live."""
+# encoding=utf-8
+from plone import api
+from plone.autoform import directives
+from plone.autoform.interfaces import IFormFieldProvider
+from plone.supermodel import model
+from plone.supermodel.directives import fieldset
+from zope import schema
+from zope.interface import Interface
+from zope.interface import provider
 
-from zope.publisher.interfaces.browser import IDefaultBrowserLayer
+class IVotableLayer(Interface):
+    """Marker interface for the Browserlayer
+    """
 
+# Ivotable is the marker interface for contenttypes who support this behavior
+class IVotable(Interface):
+    pass
 
-class IStarzelVotableBehaviorLayer(IDefaultBrowserLayer):
-    """Marker interface that defines a browser layer."""
+# This is the behaviors interface. When doing IVoting(object), you receive an
+# adapter
+@provider(IFormFieldProvider)
+class IVoting(model.Schema):
+    if not api.env.debug_mode():
+        directives.omitted("votes")
+        directives.omitted("voted")
+
+    fieldset(
+        'debug',
+        label=u'debug',
+        fields=('votes', 'voted'),
+    )
+
+    votes = schema.Dict(title=u"Vote info",
+                        key_type=schema.TextLine(title=u"Voted number"),
+                        value_type=schema.Int(title=u"Voted so often"),
+                        required=False)
+    voted = schema.List(title=u"Vote hashes",
+                        value_type=schema.TextLine(),
+                        required=False)
+
+    def vote(request):
+        """
+        Store the vote information, store the request hash to ensure
+        that the user does not vote twice
+        """
+
+    def average_vote():
+        """
+        Return the average voting for an item
+        """
+
+    def has_votes():
+        """
+        Return whether anybody ever voted for this item
+        """
+
+    def already_voted(request):
+        """
+        Return the information wether a person already voted.
+        This is not very high level and can be tricked out easily
+        """
+
+    def clear():
+        """
+        Clear the votes. Should only be called by admins
+        """
