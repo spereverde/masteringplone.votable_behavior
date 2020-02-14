fcecd7846eff35a6afd817b91d727ff7eaabe72d
Kim Paulissen <kim.paulissen@kuleuven.be>
2020-02-14 13:31:58 +0100

    add behavior implementation - part 2

--- src/starzel/votable_behavior/behavior/voting.py
+++ src/starzel/votable_behavior/behavior/voting.py
@@ -28,3 +28,46 @@ class Vote(object):
     @property
     def voted(self):
         return self.annotations['voted']
+
+    def _hash(self, request):
+        """
+        This hash can be tricked out by changing IP addresses and might allow
+        only a single person of a big company to vote
+        """
+        hash_ = md5()
+        hash_.update(request.getClientAddr())
+        for key in ["User-Agent", "Accept-Language", "Accept-Encoding"]:
+            hash_.update(request.getHeader(key))
+        return hash_.hexdigest()
+
+    def vote(self, vote, request):
+        if self.already_voted(request):
+            raise KeyError("You may not vote twice")
+        vote = int(vote)
+        self.annotations['voted'].append(self._hash(request))
+        votes = self.annotations['votes']
+        if vote not in votes:
+            votes[vote] = 1
+        else:
+            votes[vote] += 1
+
+    def average_vote(self):
+        if not has_votes(self):
+            return 0
+        total_votes = sum(self.annotations['votes'].values())
+        total_points = sum(
+            [vote * count for (vote, count) in self.annotations['votes'].items()])
+        return float(total_points) / total_votes
+
+    def has_votes(self):
+        return len(self.annotations.get('votes', [])) != 0
+
+    def already_voted(self, request):
+        return self._hash(request) in self.annotations['voted']
+
+    def clear(self):
+        annotations = IAnnotations(self.context)
+        annotations[KEY] = PersistentDict(
+            {'voted': PersistentList(), 'votes': PersistentDict()}
+        )
+        self.annotations = annotations[KEY]
